feb0059db92d5917647743dd9bd324d07b9dddb4
EunseokYang2 <141837524+EunseokYang2@users.noreply.github.com>
2023-09-01 10:15:02 +0900

    Update DA_engine.py

--- O2net-main/O2net/DA_engine.py
+++ O2net-main/O2net/DA_engine.py
@@ -173,7 +173,6 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module, args,
             loss_global_da += global_DA_img_loss
         loss_dict["loss_da"] = args.instance_loss_coef * loss_da + loss_global_da
         loss_dict["loss_wasserstein"] = swd(hs_src[-1], hs_tgt[-1])
-        a = loss_dict["loss_wasserstein"]
         #loss_dict["loss_l2_norm"] = l2_norm(hs_src[-1], hs_tgt[-1])
 
         losses = sum(loss_dict[k] * weight_dict[k]
@@ -218,7 +217,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module, args,
     # gather the stats from all processes
     metric_logger.synchronize_between_processes()
     print("Averaged stats:", metric_logger)
-    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}, a
+    return {k: meter.global_avg for k, meter in metric_logger.meters.items()}
 
 
 @torch.no_grad()
